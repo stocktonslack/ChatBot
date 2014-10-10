5e69eac90d5270d9f580f894fcec4314fdf6eb3b
Stockton Slack <stocktonslack@gmail.com>
2014-10-10 09:51:37 -0600

    updated documentation

--- src/chatbot/controller/ChatbotRunner.java
+++ src/chatbot/controller/ChatbotRunner.java
@@ -3,6 +3,10 @@ package chatbot.controller;
 public class ChatbotRunner
 {
 
+	/**
+	 * creates and runs the Chatbot
+	 * @param args
+	 */
 	public static void main(String[] args)
 	{
 		ChatbotAppController appController = new ChatbotAppController();
--- src/chatbot/model/Chatbot.java
+++ src/chatbot/model/Chatbot.java
@@ -8,80 +8,87 @@ public class Chatbot
 	 * The content that is input from the user, for checking
 	 */
 	private String contentArea;
-	
+
 	/**
 	 * The name of the chatBot
 	 */
 	private String name;
-	
+
 	/**
 	 * Sets the number of Chats and increases it by one each time.
 	 */
 	private int numberOfChats;
-	
+
 	/**
 	 * This creates an ArrayList called memeList, for the memes to be put into.
 	 */
 	private ArrayList<String> memeList;
 	
+	private String stringArea;
+
 	/**
-	 * Sets the name of the Chatbot and creates the array for the memeList, the contentList.
-	 * @param name The name of the Chatbot
+	 * Sets the name of the Chatbot and creates the array for the memeList, the
+	 * contentList.
+	 * @param name
+	 *            The name of the Chatbot
 	 */
 	public Chatbot(String name)
 	{
 		this.name = name;
 		numberOfChats = 0;
-		
-		//this. means talk to the current class
 		contentArea = "";
+		stringArea = "";
 		memeList = new ArrayList<String>();
 		fillTheMemeList();
-		
+		// this. means talk to the current class
 	}
-	
+
 	/**
 	 * Returns the name of the chatbot
+	 * 
 	 * @return returns the name of the chatbot
 	 */
 	public String getName()
 	{
 		return name;
-		//can be return this.name there is no difference
+		// can be return this.name there is no difference
 	}
-	
+
 	/**
 	 * Returns the number of chats for this chatbot.
+	 * 
 	 * @return the current number of chats
 	 */
 	public int getNumbeOfChats()
 	{
 		return numberOfChats;
 	}
-	
+
 	/**
 	 * This allows anyone to change the chatbot name.
-	 * @param name the new name for the chatbot
+	 * 
+	 * @param name
+	 *            the new name for the chatbot
 	 */
 	public void setName(String name)
 	{
 		this.name = name;
-		//allows anyone to change the Chatbot name if they call setName
+		// allows anyone to change the Chatbot name if they call setName
 	}
-	
+
 	/**
 	 * This adds one to the number of chats.
 	 */
 	public void incrementChats()
 	{
 		numberOfChats++;
-		//++ is a shortcut to add 1 to a variable
+		// ++ is a shortcut to add 1 to a variable
 	}
-	
+
 	/**
 	 * Fills the contents of the memeList with internet memes.
 	 */
-	private void fillTheMemeList()	
+	private void fillTheMemeList()
 	{
 		memeList.add("y u mad bro");
 		memeList.add("I'm always watching you... alway's watching you...");
@@ -90,109 +97,139 @@ public class Chatbot
 		memeList.add("one does not simply");
 		memeList.add("Mormon Memes");
 		memeList.add("You Shall Not PASS)");
-		
+
 	}
-	
+
 	/**
-	 * processed the supplied text from the user to provide a message from the Chatbot.
-	 * @param userText The user supplied text.
+	 * processed the supplied text from the user to provide a message from the
+	 * Chatbot.
+	 * 
+	 * @param userText
+	 *            The user supplied text.
 	 * @return What the Chatbot says because of the supplied input.
 	 */
 	public String processText(String userText)
 	{
 		String processedText = "";
 		incrementChats();
-		
-		int randomChoice= (int)(Math.random() * 3);
-		
-		if(randomChoice == 0)
+
+		int randomChoice = (int) (Math.random() * 3);
+		if (userText != null)
 		{
-			if(contentChecker(userText))
+
+		}
+
+		/**
+		 * if a random number is 0 then it will use the string checker
+		 */
+		if (randomChoice == 0)
+		{
+			if (stringChecker(userText))
 			{
-				processedText = "No way! Let's talk about " + userText;
-				processedText +=" I reallly like that";
+				processedText = "that was to long, sorry!";
 			}
 			else
 			{
-				processedText = "We Should talk about something else";
+				processedText = "Short is goodish";
 			}
 		}
+
+		/**
+		 * if a random number is 1 then it will use the contentChecker
+		 */
 		else if (randomChoice == 1)
 		{
-			
-		}
-		else
-		{	
-			if(memeChecker(userText))
+			if (contentChecker(userText))
 			{
-			processedText = "Guess What! You found a meme: " + userText;
-			processedText += " Isn't that sweet!";
+				processedText = "The secret involves" + userText;
 			}
 			else
 			{
-			processedText = "I'm sad, that wasn't a meme.";
+				if (memeChecker(userText))
+				{
+					processedText = "Guess What! You found a meme: " + userText;
+					processedText += " Isn't that sweet!";
+				}
+				else
+				{
+					processedText = "I'm sad, that wasn't a meme.";
+				}
 			}
-		
-		return processedText;
 		}
+		return processedText;
 	}
-	
+
 	/**
 	 * Checks to see if it is a meme
-	 * @param currentText  Checks the users input text
+	 * 
+	 * @param currentText
+	 *            Checks the users input text
 	 * @return returns true or false if the meme is in the list or not.
 	 */
 	private boolean memeChecker(String currentText)
 	{
-		boolean isAMeme =  false;
-		
-		for(String currentMeme : memeList)
+		boolean isAMeme = false;
+
+		for (String currentMeme : memeList)
 		{
-			if(currentMeme.equalsIgnoreCase(currentText))
+			if (currentMeme.equalsIgnoreCase(currentText))
 			{
 				isAMeme = true;
 			}
 		}
-		
+
 		return isAMeme;
 	}
-	
+
 	/**
-	 * Check's the content from the user to see if it talks about any certain subjects
-	 * @param input If what the user put's in is in the contentArea, then it will return true.
+	 * Check's the content from the user to see if it talks about any certain
+	 * subjects
+	 * 
+	 * @param input
+	 *            If what the user put's in is in the contentArea, then it will
+	 *            return true.
 	 * @return returns the isContent, after it is true.
 	 */
 	private boolean contentChecker(String input)
 	{
 		boolean isContent = false;
-		
-		if(input.contains(contentArea))
+
+		if (input.contains(contentArea))
 		{
 			isContent = true;
 		}
-		
+
 		return isContent;
 	}
 
 	private boolean stringChecker(String input)
 	{
-		boolean isTooLong
+		boolean isTooLong = false;
+		
+		if (input.contains(stringArea))
+		{
+			isTooLong = true;
+		}
+		
+		return isTooLong;
 	}
-	
+
 	/**
 	 * Check's to see if you want to quit
-	 * @param input determines what you put into the box
+	 * 
+	 * @param input
+	 *            determines what you put into the box
 	 * @return returns whether or not it is okay to quit
 	 */
 	public boolean quitChecker(String input)
 	{
 		boolean okToQuit = false;
-		
-		if(input != null && input.equals("quit"))
+
+		if (input != null && input.equals("quit"))
 		{
 			okToQuit = true;
 		}
-		
+
 		return okToQuit;
 	}
 }
