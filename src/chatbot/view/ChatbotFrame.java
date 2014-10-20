71a5ca5fd0c197e3cdb54b654fc48b395c6be9fb
Stockton Slack <stocktonslack@gmail.com>
2014-10-20 09:55:44 -0600

    more GUI and add Docs
    adding more GUI and documentation

--- src/chatbot/view/ChatbotFrame.java
+++ src/chatbot/view/ChatbotFrame.java
@@ -4,21 +4,37 @@ import javax.swing.JFrame;
 
 import chatbot.controller.ChatbotAppController;
 
+/**
+ * The chatbotFrame takes some of it's characteristics from the JFrame.
+ * @author ssla9721
+ *
+ */
 public class ChatbotFrame extends JFrame
 {
+	/**
+	 * creates the basePanel so that you can use it and set it's characteristics from the frame.
+	 */
 	private ChatbotPanel basePanel;
-	
-	public ChatbotFrame(ChatbotAppController baseController) 
+
+	/**
+	 * creates the chatbotFrame with the parameters inside the AppController and baseController
+	 * @param baseController
+	 */
+	public ChatbotFrame(ChatbotAppController baseController)
 	{
 		basePanel = new ChatbotPanel(baseController);
 		setupFrame();
 	}
 
+	/**
+	 * The basic setup and characteristics for the frame.
+	 */
 	private void setupFrame()
 	{
 		this.setContentPane(basePanel);
 		this.setSize(400, 400);
+		this.setResizable(false);
 		this.setVisible(true);
 	}
-	
+
 }
--- src/chatbot/view/ChatbotPanel.java
+++ src/chatbot/view/ChatbotPanel.java
@@ -1,37 +1,65 @@
 package chatbot.view;
 
+import java.awt.Color;
+import java.awt.Component;
+
+import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SpringLayout;
 
 import chatbot.controller.ChatbotAppController;
 
+/**
+ * the ChatbotPanel gets characteristics from the JPanel
+ * 
+ * @author ssla9721
+ *
+ */
 public class ChatbotPanel extends JPanel
 {
-		private ChatbotAppController baseController;
-		
-		public ChatbotPanel(ChatbotAppController baseController)
-		{
-			this.baseController = baseController;
-			
-			setupPanel();
-			setupLayout();
-			setupListeners();
-			
-		}
-		
-		private void setupPanel()
-		{
-			
-		}
-		
-		private void setupLayout()
-		{
-			
-		}
-		
-		private void setupListeners()
-		{
-			
-		}
+	private ChatbotAppController baseController;
+	private JButton sampleButton;
+	private JTextField sampleField;
+
+	private SpringLayout baseLayout;
+
+	public ChatbotPanel(ChatbotAppController baseController)
+	{
+		this.baseController = baseController;
+
+		sampleButton = new JButton("click on me");
+		sampleField = new JTextField(25);
+
+		baseLayout = new SpringLayout();
+
+		setupPanel();
+		setupLayout();
+		setupListeners();
+
+	}
+
+	private void setupPanel()
+	{
+		this.setBackground(Color.ORANGE);
+
+		this.setLayout(baseLayout);
+
+		this.add(sampleButton);
+		this.add(sampleField);
+
+	}
+
+	private void setupLayout()
+	{
+		baseLayout.putConstraint(SpringLayout.WEST, sampleField, 84, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.SOUTH, sampleField, 0, SpringLayout.SOUTH, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, sampleButton, 5, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.EAST, sampleButton, -289, SpringLayout.EAST, this);
+	}
+
+	private void setupListeners()
+	{
+
+	}
 }
-
-
